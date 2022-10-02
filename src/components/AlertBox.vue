15c27972b486f7b120717dfc310fad6594251d79
adrien <adrien.charrier@outlook.fr>
2022-10-02 14:17:40 +0200

    FEAT - popup to display errors

---
+++ src/components/AlertBox.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="alert-box" :class="alertBoxClasses" v-if="show">
+    <div class="alert-box__content">
+      <slot></slot>
+    </div>
+    <button class="alert-box__close" @click="closeAlert">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-x"
+      >
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, toRef, watch } from 'vue';
+
+const props = defineProps<{
+  type: 'success' | 'error' | 'warning' | 'info';
+  show: boolean;
+}>();
+
+const show = toRef(props, 'show');
+
+const emit = defineEmits(['close']);
+
+const closeAlert = () => {
+  emit('close');
+  show.value = false;
+};
+
+const alertBoxClasses = reactive({
+  'alert-box--success': false,
+  'alert-box--error': false,
+  'alert-box--warning': false,
+  'alert-box--info': false,
+});
+const k = `alert-box--${props.type}`;
+
+if (k in alertBoxClasses) {
+  alertBoxClasses[k as keyof typeof alertBoxClasses] = true;
+}
+</script>
+
+<style lang="scss" scoped>
+.alert-box {
+  position: relative;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+}
+
+.alert-box--success {
+  background-color: #d4edda;
+  border-color: #c3e6cb;
+  color: #155724;
+}
+
+.alert-box--error {
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+  color: #721c24;
+}
+
+.alert-box--warning {
+  background-color: #fff3cd;
+  border-color: #ffeeba;
+  color: #856404;
+}
+
+.alert-box--info {
+  background-color: #d1ecf1;
+  border-color: #bee5eb;
+  color: #0c5460;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,38 +8,65 @@
       <input type="password" id="password" v-model="password" />
       <button type="submit" @click.prevent="sendLogin">Login</button>
     </form>
+
+    <AlertBox type="info" :show="showError" @close="showError = false">{{
+      errorTxt
+    }}</AlertBox>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue';
+import AlertBox from '@/components/AlertBox.vue';
 
-const login = ref("");
-const password = ref("");
+const login = ref('');
+const password = ref('');
+
+const showError = ref(false);
+const errorTxt = ref('');
 
 const sendLogin = () => {
-  fetch("http://localhost:80/api/login_check", {
-    method: "POST",
+  fetch('http://localhost:80/api/login_check', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       pseudo: login.value,
       password: password.value,
     }),
   })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        console.log(res);
+    .then((response) => {
+      if (!response.ok) {
+        throw response;
       }
+      return response.json();
     })
     .then((data) => {
       console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      showErrors(error);
     });
 };
 
+const showErrors = (error: unknown) => {
+  if (error instanceof Error) {
+    console.log('error instanceof Error');
+    console.log(error.message);
+    errorTxt.value = error.message;
+    showError.value = true;
+  }
+  if (error instanceof Response) {
+    console.log('error instanceof Response');
+    error.json().then((data) => {
+      console.log(data);
+      errorTxt.value = data.message;
+      showError.value = true;
+    });
+  }
+};
 </script>
 
 <style scoped></style>
